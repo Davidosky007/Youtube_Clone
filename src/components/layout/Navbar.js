6f3a8f86b75787998775bbecaa0c0ad21b72b7f2
Ranacode <romerodeveloper@techie.com>
2018-12-15 20:13:36 +0000

    Setup router, layout folder and firebase project configuration

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Navbar extends Component {
+  state = {};
+
+  componentDidMount() {}
+  render() {
+    return <div>Navbar</div>;
+  }
+}
+
+export default Navbar;
